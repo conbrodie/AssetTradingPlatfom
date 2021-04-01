0c188ce9f42b3513af71fa268773d74cc69f1925
Tarrant Cauchi <tac@kedstar.com>
2021-04-02 02:54:36 +1000

    Added more detail to all class methods in JavaDoc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"AssetTradingPlatform","l":"Asset"},{"p":"AssetTradingPlatform","l":"Market"},{"p":"AssetTradingPlatform","l":"Unit"},{"p":"AssetTradingPlatform","l":"User"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"AssetTradingPlatform","l":"Asset"},{"p":"AssetTradingPlatform","l":"Market"},{"p":"AssetTradingPlatform","l":"Order"},{"p":"AssetTradingPlatform","l":"Unit"},{"p":"AssetTradingPlatform","l":"User"}];updateSearchResults();
