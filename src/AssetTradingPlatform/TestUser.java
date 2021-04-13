91b94a6bf1542eecb69e7a7b9b040fb7d5b24b00
Kem <lathikemly@gmail.com>
2021-04-13 14:58:13 +1000

    Added some test cases for Login Method

--- src/AssetTradingPlatform/TestUser.java
+++ src/AssetTradingPlatform/TestUser.java
@@ -14,76 +14,112 @@ public class TestUser {
         userDB = new MockUserDB();
         user = new User(userDB);
     }
+    /**
+     * Test for createUser Method
+     */
+//    @Test
+//    public void testSuccessCreateUser() throws CredentialException {
+//        user.createUser("Kate", "123", "kate@gmail.com","ComputerClusterDivision");
+//        assertEquals("Logged in", user.Login("Kate","123"), "Failed to create user") ;
+//    }
+//    @Test
+//    public void testAddingSpecialCharacters() throws CredentialException {
+//        user.createUser("Kate@90", "123", "1@yahoo.com","ComputerClusterDivision");
+//        assertEquals("Logged in", user.Login("Kate@90","123"),"Creating user failed");
+//    }
+//    @Test
+//    public void testGivingMaxStringLengthOfUserName() throws CredentialException {
+//        user.createUser("kkkkkkkkkkkkkkkk", "1", "kate123@gmail.com","ComputerClusterDivision");
+//        assertEquals("Logged in", user.Login("kkkkkkkkkkkkkkkk","1"),"Creating user failed");
+//
+//    }
+//    @Test
+//    public void testTheSameOfUseNameAndPassword() throws CredentialException {
+//        user.createUser("Kate123", "Kate123", "kate@gmail.com","ComputerClusterDivision");
+//        assertEquals("Logged in", user.Login("Kate123","Kate123"),"Creating user failed");
+//    }
+//    @Test
+//    public void testLeaveUserNameBlank(){
+//        assertThrows(CredentialException.class, () ->{
+//            user.createUser("", "123", "kate@gmail.com","ComputerClusterDivision");
+//        });
+//    }
+//    @Test
+//    public void testLeavePasswordBlank(){
+//        assertThrows(CredentialException.class, () ->{
+//            user.createUser("Kate", "", "kate@gmail.com","ComputerClusterDivision");
+//        });
+//    }
+//    @Test
+//    public void testLeaveEmailBlank(){
+//        assertThrows(CredentialException.class, () ->{
+//            user.createUser("Kate", "123", "","ComputerClusterDivision");
+//        });
+//    }
+//    @Test
+//    public void testLeaveOrganisationBlank(){
+//        assertThrows(CredentialException.class, () ->{
+//            user.createUser("Kate", "123", "kate@gmail.com","");
+//        });
+//    }
+//    @Test
+//    public void testTooLongStringLength(){
+//        assertThrows(CredentialException.class, () -> {
+//            user.createUser("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "123", "kate@gmail.com","ComputerClusterDivision");
+//        });
+//    }
+//    @Test
+//    public void testUsernameAlreadyExist(){
+//        assertThrows(CredentialException.class, () -> {
+//            user.createUser("Kate", "123", "kate@gmail.com","ComputerClusterDivision");
+//            user.createUser("Kate", "1", "kate1@gmail.com","HR Management");
+//        });
+//    }
+//    @Test
+//    public void testInvalidUserName(){
+//        assertThrows(CredentialException.class, () -> {
+//            user.createUser("kate:&/", "123", "kate.12","ComputerClusterDivision");
+//        });
+//    }
+//    @Test
+//    public void testInvalidEmail(){
+//        assertThrows(CredentialException.class, () -> {
+//            user.createUser("Kate", "123", "kate.12","ComputerClusterDivision");
+//        });
+//    }
+    /**
+     * Test for Login Method
+     */
     @Test
-    public void testSuccessCreateUser() throws CredentialException {
+    public void testSuccessLogin() throws CredentialException {
         user.createUser("Kate", "123", "kate@gmail.com","ComputerClusterDivision");
-        assertEquals("Logged in", user.Login("Kate","123"), "Creating user failed") ;
+        assertEquals("Logged in", user.Login("Kate","123"), "Failed to login");
     }
     @Test
-    public void testAddingSpecialCharacters() throws CredentialException {
-        user.createUser("Kate@90", "123", "1@yahoo.com","ComputerClusterDivision");
-        assertEquals("Logged in", user.Login("Kate@90","123"),"Creating user failed");
-    }
-    @Test
-    public void testGivingMaxStringLengthOfUserName() throws CredentialException {
-        user.createUser("kkkkkkkkkkkkkkkk", "1", "kate123@gmail.com","ComputerClusterDivision");
-        assertEquals("Logged in", user.Login("kkkkkkkkkkkkkkkk","1"),"Creating user failed");
-
-    }
-    @Test
-    public void testTheSameOfUseNameAndPassword() throws CredentialException {
-        user.createUser("Kate123", "Kate123", "kate@gmail.com","ComputerClusterDivision");
-        assertEquals("Logged in", user.Login("Kate123","Kate123"),"Creating user failed");
-    }
-    @Test
-    public void testLeaveUserNameBlank(){
+    public void testLeaveNameBlank(){
         assertThrows(CredentialException.class, () ->{
-            user.createUser("", "123", "kate@gmail.com","ComputerClusterDivision");
+            user.Login("","123");
         });
     }
     @Test
-    public void testLeavePasswordBlank(){
+    public void testLeavePwBlank(){
         assertThrows(CredentialException.class, () ->{
-            user.createUser("Kate", "", "kate@gmail.com","ComputerClusterDivision");
+            user.Login("Kate","");
         });
     }
     @Test
-    public void testLeaveEmailBlank(){
+    public void testIncorrectUserName(){
         assertThrows(CredentialException.class, () ->{
-            user.createUser("Kate", "123", "","ComputerClusterDivision");
-        });
-    }
-    @Test
-    public void testLeaveOrganisationBlank(){
-        assertThrows(CredentialException.class, () ->{
-            user.createUser("Kate", "123", "kate@gmail.com","");
-        });
-    }
-    @Test
-    public void testTooLongStringLength(){
-        assertThrows(CredentialException.class, () -> {
-            user.createUser("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "123", "kate@gmail.com","ComputerClusterDivision");
-        });
-    }
-    @Test
-    public void testUsernameAlreadyExist(){
-        assertThrows(CredentialException.class, () -> {
             user.createUser("Kate", "123", "kate@gmail.com","ComputerClusterDivision");
-            user.createUser("Kate", "1", "kate1@gmail.com","HR Management");
+            user.Login("Kate1","123");
         });
     }
     @Test
-    public void testInvalidUserName(){
-        assertThrows(CredentialException.class, () -> {
-            user.createUser("kate:&/", "123", "kate.12","ComputerClusterDivision");
+    public void testIncorrectPassword(){
+        assertThrows(CredentialException.class, () ->{
+            user.createUser("Kate", "123", "kate@gmail.com","ComputerClusterDivision");
+            user.Login("Kate","1234");
         });
     }
-    @Test
-    public void testInvalidEmail(){
-        assertThrows(CredentialException.class, () -> {
-            user.createUser("Kate", "123", "kate.12","ComputerClusterDivision");
-        });
-    }
-
 }
 
