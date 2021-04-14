f00bddc82830239723d12b6db1a66fd71b26a77f
Chris Stock <chris.r.stock@gmail.com>
2021-04-14 13:20:06 +1000

    Started Client GUI
    Just started to make some of the GUI to try it out.

---
+++ src/client/client.java
@@ -0,0 +1,15 @@
+package client;
+
+import javax.swing.*;
+
+public class client {
+    private JPanel form_main;
+    private JTabbedPane userpane;
+    private JPanel userpanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+}
