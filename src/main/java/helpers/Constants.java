b0249f65093c2564f4be0d6d61eb67d2242f9f1e
Chris Stock <chris.r.stock@gmail.com>
2021-04-17 16:01:17 +1000

    Added some files to Assist with Server connections

---
+++ src/main/java/helpers/Constants.java
@@ -0,0 +1,10 @@
+package helpers;
+
+public class Constants {
+    /**
+     * used for holding application constants
+     */
+
+    // Constant for msg header
+    public final static int HEADER_SIZE = 4; // bytes
+}
