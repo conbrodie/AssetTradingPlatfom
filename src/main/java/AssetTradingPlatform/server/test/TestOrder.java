8c73e4e3275b2eff1254624643e9e4be993d5713
Kem <lathikemly@gmail.com>
2021-04-22 11:21:00 +1000

    Added testcase for createOrder Method

--- src/main/java/AssetTradingPlatform/server/test/TestOrder.java
+++ src/main/java/AssetTradingPlatform/server/test/TestOrder.java
@@ -20,12 +20,95 @@ public class TestOrder {
         order = new OrderController(DB);
     }
     /**
-     * Test for Create New Order Method
+     * Test for Create Buy Method
      */
     @Test
     public void  testSuccessCreateBuy() throws Exception {
         Order b = order.createOrder("CPU Hours", OrderRequest.BUY, 50, 10, "ComputeClusterDivision");
         assertEquals( new Order(1, LocalDateTime.now(), "CPU Hours", OrderRequest.BUY, 50, 10), b,"Failed to create a buy");
+    }
+    @Test
+    public void  testBuyMinOfQuantityAndPrice() throws Exception {
+        Order b = order.createOrder("CPU Hours", OrderRequest.BUY, 50, 10, "ComputeClusterDivision");
+        assertEquals( new Order(1, LocalDateTime.now(), "CPU Hours", OrderRequest.BUY, 1, 1), b,"Failed to create a buy");
+    }
+    @Test
+    public void  testBuyMaxOfQuantityAndPrice() throws Exception {
+        Order b = order.createOrder("CPU Hours", OrderRequest.BUY, 50, 10, "ComputeClusterDivision");
+        assertEquals( new Order(1, LocalDateTime.now(), "CPU Hours", OrderRequest.BUY, 2147483647, 2147483647), b,"Failed to create a buy");
+    }
+    @Test
+    public void  testBuyPriceIsZero()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.BUY, 50, 0, "ComputeClusterDivision");
+        });
+    }
+    @Test
+    public void  testBuyQuantityIsZero()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.BUY, 0, 10, "ComputeClusterDivision");
+        });
+    }
+    @Test
+    public void  testBuyQuantityIsTooBig()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.BUY, 2147483648, 10, "ComputeClusterDivision");
+        });
+    }
+    @Test
+    public void  testBuyQuantityIsNegative()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.BUY, -10, 10, "ComputeClusterDivision");
+        });
+    }
+    /**
+     * Test for Create Sell Method
+     */
+    @Test
+    public void  testSuccessCreateSell() throws Exception {
+        Order b = order.createOrder("CPU Hours", OrderRequest.SELL, 50, 10, "ComputeClusterDivision");
+        assertEquals( new Order(1, LocalDateTime.now(), "CPU Hours", OrderRequest.SELL, 50, 10), b,"Failed to create a sell");
+    }
+    @Test
+    public void  testSellMinOfQuantityAndPrice() throws Exception {
+        Order b = order.createOrder("CPU Hours", OrderRequest.SELL, 50, 10, "ComputeClusterDivision");
+        assertEquals( new Order(1, LocalDateTime.now(), "CPU Hours", OrderRequest.SELL, 1, 1), b,"Failed to create a sell");
+    }
+    @Test
+    public void  testSellMaxOfQuantityAndPrice() throws Exception {
+        Order b = order.createOrder("CPU Hours", OrderRequest.SELL, 50, 10, "ComputeClusterDivision");
+        assertEquals( new Order(1, LocalDateTime.now(), "CPU Hours", OrderRequest.SELL, 2147483647, 2147483647), b,"Failed to create a sell");
+    }
+    @Test
+    public void  testSellPriceIsZero()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.SELL, 50, 0, "ComputeClusterDivision");
+        });
+    }
+    @Test
+    public void  testSellQuantityIsZero()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.SELL, 0, 10, "ComputeClusterDivision");
+        });
+    }
+    @Test
+    public void  testSellQuantityIsTooBig()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.SELL, 2147483648, 10, "ComputeClusterDivision");
+        });
+    }
+//    @Test
+//    public void  testSellAmountIsBiggerThanUnitHas()  {
+//        assertThrows(CredentialException.class, () -> {
+//
+//            Order b = order.createOrder("CPU Hours", OrderRequest.SELL, 60, 10, "ComputeClusterDivision");
+//        });
+//    }
+    @Test
+    public void  testSellQuantityIsNegative()  {
+        assertThrows(CredentialException.class, () -> {
+            Order b = order.createOrder("CPU Hours", OrderRequest.SELL, -10, 10, "ComputeClusterDivision");
+        });
     }
 
 }
