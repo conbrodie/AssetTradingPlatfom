5b2b30774a23d50cb2f8ddf2c704e878fa110093
Tarrant Cauchi <tac@kedstar.com>
2021-04-24 23:06:13 +1000

    Updated Unit field names and types to match DB Cleaned up JavaDoc

--- src/main/java/AssetTradingPlatform/common/Unit.java
+++ src/main/java/AssetTradingPlatform/common/Unit.java
@@ -1,84 +1,87 @@
 package AssetTradingPlatform.common;
 
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 /**
- * A class to manage an OU (Organisational Unit) details and functions related to OUs
+ * A class to create and manage an OU (Organisational Unit)
  */
 public class Unit {
-    public String unitName;
-    public float credits;
-    public TreeSet ownedAssets;
+    public int org_unit_id;
+    public String org_unit_name;
+    public int credits;
+    /**
+     * Temporary set to store a units assets and their amounts until connected to DB
+     */
+    public TreeMap org_assets;
 
 
     /**
      * Create a new Organisational Unit
-     * A new unit is created and saved to a database of units
-     * @param unitName New unit name
+     * New unit is created and saved to database
+     * @param org_unit_name New unit name
      * @throws Exception Unit name already exists
      */
-    public Unit(String unitName) throws Exception{
+    public Unit(String org_unit_name) throws Exception{
 
     }
 
 
     /**
      * Add User to Unit
-     * @param userName User to add
-     * @param unitName Unit to add to
+     * @param username User to add
+     * @param org_unit_name Unit to add to
      * Check if unit leader. (Does user have permission to perform action)
      * Can user be part of multiple units?
-     * @throws Exception Username doesnt exist
-     * @throws Exception Unitname doesnt exist
-     * @throws Exception User already in unit
+     * @throws Exception username doesn't exist
+     * @throws Exception org_unit_name doesn't exist
+     * @throws Exception username already in unit
      */
-    public void addUser(String userName, String unitName) throws Exception{
+    public void addUser(String username, String org_unit_name) throws Exception{
 
     }
 
 
     /**
      * Remove User from Unit
-     * @param userName User to remove
-     * @param unitName Unit to remove from
+     * @param username User to remove
+     * @param org_unit_name Unit to remove from
      * Check if unit leader. (Does user have permission to perform action)
-     * @throws Exception Username doesnt exist
-     * @throws Exception Username not part of unit
-     * @throws Exception Unitname doesnt exist
+     * @throws Exception username doesn't exist
+     * @throws Exception username not part of unit
+     * @throws Exception org_unit_name doesn't exist
      */
-    public void removeUser(String userName, String unitName) throws Exception{
+    public void removeUser(String username, String org_unit_name) throws Exception{
 
     }
 
 
     /**
      * Edit unit credits
-     * @param unitName Unit to remove from
-     * Check if admin (Does user have permission to perform action)
-     * @creditAmount Amount to update units credits
-     * @see User#AdminUser Called to check that user performing edit has required admin privlidges
-     * @throws Exception User doesnt have permission
-     * @throws Exception Unitname doesnt exist
-     * @throws Exception Cant set credits to negative
+     * @param org_unit_name Unit to remove from
+     * @param credits Amount to update units credits
+     * @see AssetTradingPlatform.server.UserController#adminUser Called to check that user performing edit has required admin privlidges
+     * @throws Exception username doesn't have permission
+     * @throws Exception org_unit_name doesn't exist
+     * @throws Exception Can't set credits to negative
      */
-    public void editCredits(String unitName, float creditAmount) throws Exception{
+    public void editCredits(String org_unit_name, float credits) throws Exception{
 
     }
 
 
     /**
      * Edit unit assets
-     * @param unitName Unit to remove from
-     * Check if admin (Does user have permission to perform action)
+     * @param org_unit_name Unit to remove from
      * @param asset Asset to modify
      * @param assetAmount Update quantity of asset to this amount
-     * @see User#AdminUser Called to check that user performing edit has required admin privlidges
+     * @see AssetTradingPlatform.server.UserController#adminUser Called to check that user performing edit has required admin privlidges
      * @throws Exception User doesn't have permission
-     * @throws Exception Unit name doesn't exist
+     * @throws Exception org_unit_name doesn't exist
      * @throws Exception Cant set owned asset amount to negative
      * @throws Exception Asset not owned by unit
      */
-    public void EditAssets(String unitName, String asset, float assetAmount) throws Exception{
+    public void EditAssets(String org_unit_name, String asset, int assetAmount) throws Exception{
 
     }
 }
