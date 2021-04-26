ba07302e15b63c57082bb965992b3313d37acbdd
conbrodie <warren.brodie@itdsolutions.com.au>
2021-04-26 13:44:07 +1000

    Add Base GUI Form (Sidebar, topbar and content area)

---
+++ src/main/java/GUI/Base.java
@@ -0,0 +1,15 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Base {
+    private JPanel background;
+    private JPanel sideBar;
+    private JPanel content;
+    private JPanel topBar;
+    private JButton button;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
