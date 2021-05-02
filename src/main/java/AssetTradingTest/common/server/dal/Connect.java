e543617a24f4f8d898cf6685606c0f5ac59e7c9f
Chris Stock <chris.r.stock@gmail.com>
2021-05-02 10:23:22 +1000

    Created some of the transport classes, and created some of the DAL
    Working on the connection DAL(data access layer) completed most of the Transport classes.  still requires some testing and needs more comments.

---
+++ src/main/java/AssetTradingTest/common/server/dal/Connect.java
@@ -0,0 +1,106 @@
+package AssetTradingTest.common.server.dal;
+
+import AssetTradingTest.common.Utilities;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ * Connection class - used to allow other classes to inherit methods and properties
+ * Retrieves properties from resource files - see files under resources directory
+ */
+
+public class Connect {
+    private String url;
+    private String schema;
+    private String username;
+    private String password;
+    private Connection conn = null;
+
+    /**
+     * Used to get properties on creation of object
+     */
+    public Connect() {
+        this.getProperties();
+    }
+
+    /**
+     * @action Used to read resource file db.prop
+     * @notes  database properties
+     * @param none
+     * @return set private properties
+     */
+    private void getProperties() {
+        try {
+            this.url = Utilities.getProperty(Connect.class, "db.prop", "jdbc.url");
+            this.schema = Utilities.getProperty(Connect.class, "db.prop", "jdbc.schema");
+            this.username = Utilities.getProperty(Connect.class, "db.prop", "jdbc.username");
+            this.password = Utilities.getProperty(Connect.class, "db.prop", "jdbc.password");
+        } catch (IOException ex) {
+            //
+        }
+    }
+
+    /**
+     * @action Used to open MariaDB or could be a mysql database connection
+     * @notes  public method to access private method
+     * @param none
+     * @return boolean - success or failure
+     */
+    public Boolean openDB() {
+        Boolean success = false;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            this.conn = (Connection) DriverManager.getConnection(this.url + "/" + this.schema + "?useSSL=false&" + "user=" + this.username + "&password=" + this.password);
+            success = true;
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("openDB() SQLException " + ex.getMessage());
+        }
+        catch (ClassNotFoundException ex)
+        {
+            System.out.println("openDB() ClassNotFoundException " + ex.getMessage());
+        }
+        return success;
+    }
+
+    /**
+     * @action Used to access MariaDB or mysql database connection
+     * @notes  public method to access private method
+     * @param none
+     * @return mariaDb / mysql database connection
+     */
+    public Connection getDBConnection() {
+        return this.conn;
+    }
+
+    /**
+     * @action Used to close MariaDB or mysql database connection
+     * @notes  public method to access private method
+     * @param none
+     * @return none
+     */
+    public void closeDB() {
+        try {
+            if (this.conn != null)
+                this.conn.close();
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("closeDB() " + ex.getMessage());
+        }
+    }
+
+    /**
+     * @action Used to retrieve the database schema (database name)
+     * @notes  public method to access private method
+     * @param none
+     * @return schema name
+     */
+    public String getSchema() {
+       return this.schema;
+    }
+
+}
---
+++ src/main/java/AssetTradingTest/common/transport/JSONMessageType.java
@@ -0,0 +1,19 @@
+package AssetTradingTest.common.transport;
+
+public class JSONMessageType {
+
+    private String messageType;
+
+    public JSONMessageType() { };
+
+    public JSONMessageType(String messageType) { this.messageType = messageType; }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+}
