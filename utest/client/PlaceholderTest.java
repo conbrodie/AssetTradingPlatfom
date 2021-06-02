d63e93b53fcbf4ce635a86b68f8aa3daf938b07b
Kem <lathikemly@gmail.com>
2021-06-02 11:48:26 +1000

    Adding ANT build.xml to allow running continuous integration

---
+++ utest/client/PlaceholderTest.java
@@ -0,0 +1,19 @@
+package client;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlaceholderTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void test() {
+        assertEquals(true, true, "Uhoh");
+    }
+}
