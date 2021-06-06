6cf938b95d0796074dd26b337c2fe20403896a99
TAC-01 <72425987+TAC-01@users.noreply.github.com>
2021-06-06 18:46:46 +1000

    Updated User Tests

--- utest/server/ServerLogicCommandsUserTest.java
+++ utest/server/ServerLogicCommandsUserTest.java
@@ -13,7 +13,7 @@ public class ServerLogicCommandsUserTest {
 
     @Test
     public void testCreateUserWhileNotLoggedIn() throws Exception {
-        db = new UserDbMock(Boolean.FALSE, null);
+        db = new UserDbMock(Boolean.FALSE, null,0);
         JSONAction obj = new JSONAction();
         String expected = "{\"errorMessage\":\"Password was not valid, Are you logged in!\",\"message\":null,\"success\":\"0\"}";
         assertEquals(expected, ServerLogicCommands.manageUser(false, obj, db));
@@ -21,34 +21,26 @@ public class ServerLogicCommandsUserTest {
 
     @Test
     public void testCreateUserWhileLoggedInAndInvalid() throws Exception {
-        db = new UserDbMock(Boolean.FALSE, null);
-        JSONAction obj = new JSONAction("Create_Asset", "create", new UserModel(0, "", "", 0,0), "UserModel");
+        db = new UserDbMock(Boolean.FALSE, null,0);
+        JSONAction obj = new JSONAction("Manage_User", "create", new UserModel(0, "", "", 0,0), "UserModel");
         String expected = "{\"errorMessage\":\"User '' was not created.\",\"message\":null,\"success\":\"0\"}";
         assertEquals(expected, ServerLogicCommands.manageUser(true, obj, db));
     }
 
     @Test
     public void testCreateUserWhileLoggedInAndValidResult() throws Exception {
-        db = new UserDbMock(Boolean.TRUE, null);
-        JSONAction obj = new JSONAction("Create_Asset", "create", new UserModel(302, "my.user", "verySecurePassword", 1,1), "UserModel");
+        db = new UserDbMock(Boolean.TRUE, null,0);
+        JSONAction obj = new JSONAction("Manage_User", "create", new UserModel(302, "my.user", "verySecurePassword", 1,1), "UserModel");
         String expected = "{\"errorMessage\":null,\"message\":\"User 'my.user' was created.\",\"success\":\"1\"}";
         assertEquals(expected, ServerLogicCommands.manageUser(true, obj, db));
     }
 
-
     @Test
     public void testCreateUserWhileLoggedInAndAlreadyExists() throws Exception {
-        db = new UserDbMock(Boolean.TRUE, "jones.r");
-        JSONAction obj = new JSONAction("Create_Asset", "create", new UserModel(33, "jones.r", "verySecurePassword", 1,1), "UserModel");
-        String expected = "{\"errorMessage\":\"User 'jones.r' is not available.\",\"message\":null,\"success\":\"0\"}";
+        db = new UserDbMock(Boolean.TRUE, "jones.r",1);
+        JSONAction obj = new JSONAction("Manage_User", "create", new UserModel(33, "jones.r", "verySecurePassword", 1,1), "UserModel");
+        String expected = "{\"errorMessage\":\"Username 'jones.r' is not available.\",\"message\":null,\"success\":\"0\"}";
         assertEquals(expected, ServerLogicCommands.manageUser(true, obj, db));
     }
 
-    @Test
-    public void testUserLoginWithNonExistentUsername() throws Exception {
-        db = new UserDbMock(Boolean.TRUE, null);
-        JSONAction obj = new JSONAction("Create_Asset", "create", new UserModel(302, "wrong.user", "verySecurePassword", 1,1), "UserModel");
-        String expected = "{\"errorMessage\":\"Username unknown!\",\"message\":null,\"success\":\"0\"}";
-        assertEquals(expected, ServerLogicCommands.login("wrong.user", obj, db));
-    }
 }
